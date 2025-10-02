c95a37f487f0c15791d86de01c71fd2eb2b75252
JaOliva04 <olivajake42@gmail.com>
2025-10-02 16:25:25 -0400

    fixed code so it passes tests locally

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,9 @@ using std::string;
 using std::getline;
 int main() {
 	string name;
-	cout << "Please enter your name: ";
 	getline(cin, name);
 	string str = GreetingUtils::create_message(name);
 	char* C_str = GreetingUtils::format_as_c_string(str);
-	for (int i = 0; i < str.size() + 1; ++i) {
-		cout << C_str[i];
-	}
-	cout << endl;
+	cout << C_str << endl;
 	delete[] C_str;
 }
